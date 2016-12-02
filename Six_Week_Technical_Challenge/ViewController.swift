4267053b1406d4fe8dea56ca014b0b1080ffb67d
Martinkirke1 <martinkirke1@gmail.com>
2016-12-02 09:00:56 -0700

    Initial Commit

---
+++ Six_Week_Technical_Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Six_Week_Technical_Challenge
+//
+//  Created by Martin Kirke on 12/2/16.
+//  Copyright © 2016 Ghost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
